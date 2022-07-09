5873b4a34900e659540ed2f3dc9ee81d2e33b402
Claudio Cicimurri <claudio.ccmrr@gmail.com>
2022-07-09 22:58:50 +0200

    Training interface

--- neural_net.py
+++ neural_net.py
@@ -1,11 +1,9 @@
 # This is the file containing the neural network structure
-from doctest import script_from_examples
 from tqdm import tqdm
 import numpy as np
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
 
 from data_loader import load_samples
@@ -56,6 +54,57 @@ class FacialRecognitionNetwork(nn.Module):
         x = self.linear(x)
         return x
 
+    def train(self, training_data, save_dest: str = "model.pt", epochs: int = 10, learning_rate: float = 0.001):
+        network = self.to(device)
+
+        # Create the optimizer
+        optimizer = optim.Adam(network.parameters(), lr=0.001)
+
+        # Create the loss function
+        loss_function = nn.CrossEntropyLoss()
+
+        # Train the model
+        for epoch in range(epochs):
+            print(f"Epoch {epoch}/{epochs}")
+            for i, (data, target) in enumerate(training_data):
+                data = data.to(device)
+                target = target.to(device)
+                optimizer.zero_grad()
+                output = network(data)
+                loss = loss_function(output, target)
+                loss.backward()
+                optimizer.step()
+                print(f"Batch {i} -> Loss: {loss}")
+            torch.save(network.state_dict(), save_dest)
+
+
+
+    def test(self, validation_data):
+        network = self.to(device)
+        # Test the model for validation
+        correct = 0
+        total = 0
+
+        confusion_matrix = np.zeros((7, 7), dtype=np.int32)
+
+        print("Validating model...")
+        for data, target in tqdm(zip(*validation_data), total=len(validation_data[0])):
+            data = data.view(-1, 1, 48, 48).to(device)
+            output = network(data)
+            # Get the highest index in the output
+            _, predicted = torch.max(output.data, 1)
+
+            total += 1
+            correct += int(predicted == target)
+
+            confusion_matrix[predicted][target] += 1
+        
+        print(f"Validation Accuracy: {correct/total*100:.2f}%")
+        # Show the confusion matrix
+        print("Confusion matrix:")
+        print(confusion_matrix)
+
+
 def prepare_training_data(samples_file: str, batch_size: int = 2000, sample_limit = None):
     # Load the training data
     tensor, int_emotion = load_samples(samples_file, limit=sample_limit)
@@ -82,94 +131,3 @@ def prepare_training_data(samples_file: str, batch_size: int = 2000, sample_limi
 
 
 
-def train_model():
-    # Load the prepared data
-    training_data = prepare_training_data('datasets/fer2013_train.samples', batch_size=1000)
-    
-    # Load the model from file
-    network = FacialRecognitionNetwork()
-    network.load_state_dict(torch.load('models/model.pt'), strict=False)
-    network.to(device)
-
-    # Create the optimizer
-    optimizer = optim.Adam(network.parameters(), lr=0.001)
-
-    # Create the loss function
-    loss_function = nn.CrossEntropyLoss()
-
-
-
-    # Train the model
-    num_epochs = 100
-    for epoch in range(num_epochs):
-        print(f"Epoch {epoch}/{num_epochs}")
-        for i, (data, target) in enumerate(training_data):
-            data = data.to(device)
-            target = target.to(device)
-            optimizer.zero_grad()
-            output = network(data)
-            loss = loss_function(output, target)
-            loss.backward()
-            optimizer.step()
-            print(f"Batch {i} -> Loss: {loss}")
-
-    # Save the model
-    torch.save(network.state_dict(), 'models/model.pt')
-    return network
-
-def test_model(network: FacialRecognitionNetwork):
-    network = network.to(device)
-    # Test the model for validation
-    validation_data = load_samples('datasets/fer2013_valid.samples')
-    correct = 0
-    total = 0
-
-    confusion_matrix = np.zeros((7, 7), dtype=np.int32)
-
-    print("Validating model...")
-    for data, target in tqdm(zip(*validation_data), total=len(validation_data[0])):
-        data = data.view(-1, 1, 48, 48).to(device)
-        output = network(data)
-        # Get the highest index in the output
-        _, predicted = torch.max(output.data, 1)
-
-        total += 1
-        correct += int(predicted == target)
-
-        confusion_matrix[predicted][target] += 1
-    
-    print(f"Validation Accuracy: {correct/total}")
-    # Show the confusion matrix
-    print("Confusion matrix:")
-    print(confusion_matrix)
-
-
-    # Test on the training dataset
-    training_data = load_samples('datasets/fer2013_train.samples')
-    correct = 0
-    total = 0
-
-    confusion_matrix = np.zeros((7, 7), dtype=np.int32)
-
-    print("Validating model...")
-    for data, target in tqdm(zip(*training_data), total=len(training_data[0])):
-        data = data.view(-1, 1, 48, 48).to(device)
-        output = network(data)
-        # Get the highest index in the output
-        _, predicted = torch.max(output.data, 1)
-
-        total += 1
-        correct += int(predicted == target)
-
-        confusion_matrix[predicted][target] += 1
-
-    print(f"Training Accuracy: {correct/total}")
-    # Show the confusion matrix
-    print("Confusion matrix:")
-    print(confusion_matrix)
-
-
-network = train_model()
-# network = FacialRecognitionNetwork()
-# network.load_state_dict(torch.load('models/model.pt'), strict=False)
-test_model(network)
---
+++ trainer.py
@@ -0,0 +1,143 @@
+import os
+import glob
+import datetime
+from typing import Any
+
+from data_loader import load_samples
+from neural_net import FacialRecognitionNetwork, prepare_training_data
+
+
+## Utilities
+RED_TEXT = "\033[31m"
+RESET = "\033[0m"
+
+
+def try_int(x: str, default: int = 0):
+    try:
+        return int(x)
+    except:
+        return default
+
+def choose_option(options: dict[Any, str], allow_invalid: bool = False):
+    """
+    Prompts the user to choose an option from a list of options.
+    """
+    option_keys = []
+    for i, key in enumerate(options):
+        print(f" {i+1:3}) {options[key]}")
+        option_keys.append(key)
+
+    choice = input(">>> ")
+    try:
+        choice_index = int(choice) - 1
+    except:
+        choice_index = -1
+
+    if choice_index in range(len(option_keys)):
+        return option_keys[choice_index]
+    elif allow_invalid:
+        return choice
+    else:
+        print(RED_TEXT + "Invalid choice." + RESET)
+        return choose_option(options)
+
+def list_dir(dir: str, glob_str: str = "*"):
+    """
+    Lists the contents of a directory sorted by last modified.
+    """
+    listed = sorted(os.listdir(dir), key=lambda x: -os.path.getmtime(f"{dir}/{x}"))
+    # Filter out files that don't match the glob
+    if glob_str:
+        listed = [x for x in listed if glob.fnmatch.fnmatch(x, glob_str)]
+    return listed
+
+def get_samples_file(default: str = "fer2013_train.samples"):
+    """
+    Prompts the user to choose a samples file.
+    """
+    options = { f"{x}": x for x in list_dir("datasets", "*.samples") }
+    options[default] = f"{default} (default)"
+
+    print("Enter the path to the samples:")
+    return 'datasets/' + (choose_option(options, allow_invalid=True) or default)
+
+def try_get_model_time(model: str):
+    try:
+        last_modified_time = os.path.getmtime(f'models/{model}')
+        # Make it human readable
+        return str(datetime.datetime.fromtimestamp(last_modified_time))
+    except:
+        return "???"
+
+def get_save_destination(default: str = "model.pt"):
+    """
+    Prompts the user to choose a save destination.
+    """
+    options = { f"{x}": f"[{try_get_model_time(x)}] {x}" for x in list_dir("models", "*.pt") }
+    options[default] = f"[{try_get_model_time(default)}] {default} (default)"
+
+    print("Enter the path to the save destination (or select one of the following):")
+    return 'models/' + (choose_option(options, allow_invalid=True) or default)
+
+def test_sequence(network, default: str = "fer2013_valid.samples"):
+    # Ask for the validation set
+    test_data_file = get_samples_file(default)
+    # Load the validation data
+    test_data = load_samples(test_data_file)
+
+    # Validate the network
+    network.test(test_data)
+    print("Testing complete.")
+
+
+
+def main():
+    print("Choose an option:")
+    chosen = "new_train"
+    # chosen = choose_option({
+    #     "new_train": "Train a new network",
+    #     "load_train": "Load a network and train it",
+    #     "load_test": "Load a network and test it",
+    #     "exit": "Exit"
+    # })
+
+    if chosen == "new_train":
+        # Create an empty network
+        network = FacialRecognitionNetwork()
+
+        # Ask for the training data
+        training_data_file = get_samples_file()
+
+        # Choose the save destination
+        save_dest = get_save_destination()
+
+        # Ask for the batch size
+        batch_size = try_int(input("Enter the batch size (default 1000): "), 1000)
+        # Ask for the epochs
+        epochs = try_int(input("Enter the number of epochs (default 10): "), 10)
+        # Ask for the learning rate
+        learning_rate = try_int(input("Enter the learning rate (default 0.001): "), 0.001)
+
+        # Load the training data
+        training_data = prepare_training_data(training_data_file, batch_size)
+                
+        # Train the network
+        network.train(training_data, save_dest, epochs, learning_rate)
+        print("Training complete.")
+
+        print("Do you want to validate the network? (y/n)")
+        if input(">>> ") == "y":
+            test_sequence(network)
+        
+
+    elif chosen == "load_train":
+        pass
+    elif chosen == "load_test":
+        pass
+    elif chosen == "exit":
+        return
+
+
+
+if __name__ == '__main__':
+    main()
